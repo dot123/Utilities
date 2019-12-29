9f4faad7a666b958375d17f9dc08bb88657e868b
dot123 <conjurer888888@gmail.com>
2019-12-29 18:38:07 +0800

    sublime build

--- cocos-sublime/run.bat
+++ cocos-sublime/run.bat
@@ -4,6 +4,5 @@ echo 未定义 COCOS_CONSOLE_ROOT 环境变量！
 pause  
 )
 taskkill /f /t /im HelloLua.exe  
-call "./tools/build.bat"  
-call "./simulator/win32/HelloLua.exe" 
+start ./simulator/win32/HelloLua.exe
 
